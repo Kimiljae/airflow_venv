8d2e8cce3c9f30882e648d1cb8dfd14543a33a61
Kimjaeha <rladlfwo93@gmail.com>
2025-05-24 17:15:44 +0900

    v1_t

--- airflow_rocket_T1.py
+++ airflow_rocket_T1.py
@@ -1,23 +1,26 @@
-#인터프리터 : airflow38로 변경
 import requests
 import json  
 import airflow
-from airflow import DAG
+import pendulum
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta  # 날짜/시간 처리를 위한 기본 라이브러리
+from airflow import DAG  # Airflow의 핵심 DAG 클래스
 import requests.exceptions
-from datetime import datetime, timedelta
-dag =DAG(
-  
-  dag_id = "rocket_launches", #Dag Name
-  start_date = datetime(2025,5,22,18,30), #2025년 5월 22일 18시 30분
-  schedule = '@daily',
-)
-print("DAG")
-print("DAG")
-print("DAG")
-print("DAG")
-print("DAG")
-print("DAG")
-#깃 리포지토리 초기화 - 파일추가 - 커밋 - 원격 저장소 푸시 이거 하고 잔다 내가 
-
+with DAG(
+    dag_id="airflow_rocket_T1",
+    schedule="0 0 * * *", #분,시,일,월,요일 언제 돌지 
+    start_date=pendulum.datetime(2021, 1, 1, tz="Aisa/Seoul"), #dag이 언제부터 돌건지
+    catchup=False, # 누락된 구간, 과거 날짜는 돌리지 않음 -> TRUE : 20210101 ~ 20250523 사이에 누락된 날짜가 모두 돌림
+    dagrun_timeout=datetime.timedelta(minutes=60), #60분 이상 돌면 실패
+    tags=["Table1"], 
+   #params={"example_key": "example_value"},
+) as dag:
+    bash_t1 = BashOperator(
+        task_id="bash_t1", #bash_id와 task_id 동일하게 
+        bash_command="echo whoami",
+    )
+  bash_t2 = BashOperator(
+        task_id="bash_t2", #bash_id와 task_id 동일하게 
+        bash_command="echo $HOSTNAME",
+    )
+  bash_t1 >> bash_t2
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,51 @@
 import requests
 import json  
 import airflow
-from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta  # 날짜/시간 처리를 위한 기본 라이브러리
+from airflow import DAG  # Airflow의 핵심 DAG 클래스
+from airflow.operators.python_operator import PythonOperator  # Python 함수를 실행하기 위한 Operator
+from airflow.providers.mysql.hooks.mysql import MySqlHook  # MySQL 연결을 위한 Hook
 import requests.exceptions
+dag =DAG(
+  dag_id = "rocket_launches", #Dag Name
+  start_date = datetime(2025,5,22,18,30), #2025년 5월 22일 18시 30분
+  schedule = '@daily',
+)
+download_launches = BashOperator ( #bashoperator를 이용해 curl로 URL 결과값 다운로드 
+  task_id = "download_launches", # task 이름 
+  bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0,0/launch/upcoming'", dag = dag 
+)
+#파이썬 함수는 결과값을 파싱하고 모든 로켓 사진을 다운로드
+ def _get_pictures():
+   #경로가 존재하는지 확인 
+   pathlib.path("/tmp/images").mkdir(parents=True, exist_ok=True)
+   #launches.json 파일에 있는 모든 그림 파일을 다운로드
+   with open("/tmp/launches.json") as f :
+     launches=json.load(f)
+     image_urls = [launch["image"] for launch in launches["results"]]
+     for image_url in image_urls:
+       try: 
+          response = requests.get(image_url)
+          image_filename=image_url.split("/")[-1]
+          target_file = f"/tmp/images/{image_filename}"
+          with open(target_file,"wb") as f: 
+            f.write(response.content)
+            print(f"Downloaded {image_url} to {target_file}")
+        except requests_exceptions.MissingSchema: 
+          print(f"{image_url} appears to be an invalid URL.")
+       except requests_exceptions.ConectionError: 
+          print(f"Could not connect to {image_url}.")
+
+_get_pictures = PythonOperator ( 
+  task_id = "get_pictures",
+  python_callable = _get_pictures,
+  dag = dag,                               
+)
+notify = BashOperator ( 
+  task_id = "notify",
+  bash_command= 'echo "There are not $(ls/tmp/images/ | wc -l) images."',
+  dag = dag                         
+)
+
+download_launches >> _get_pictures >> notify # 태스크 실행 순서 
